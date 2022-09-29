442309fc71dbaa567c8a6fc71e39c13493dde5b7
User <User@192.168.1.211>
2022-09-29 09:31:45 +0530

    hii

---
+++ src/test/java/scripts/test1.java
@@ -0,0 +1,13 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class test1 {
+	@Test
+	public void tc1()
+	{
+		Reporter.log("hello java",true);
+	}
+
+}
